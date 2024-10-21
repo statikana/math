cc020d8991b854281fa8f01631f3feb6bdc28c85
statikana <86629697+statikana@users.noreply.github.com>
2024-10-21 07:37:59 -0400

    Update structures.py

--- structures.py
+++ structures.py
@@ -9,9 +9,8 @@ from functools import partial
 MonoCompat = Union[int, str, "Monomial"]
 PolyCompat = Union[int, str, "Monomial", "Polynomial"]
 
-_super_list = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
-_super = str.maketrans("0123456789", "".join(_super_list))
-_super_minus = "⁻"
+_super_list = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹", "⁻"]
+_super = str.maketrans("0123456789-", "".join(_super_list))
 
 
 class Monomial:
@@ -19,24 +18,16 @@ class Monomial:
     Coeffients may be any real number.
     Powers may be any integer."""
 
-    def __init__(self, coeffient: int, **variables: int):
+    def __init__(self, coeffient: int, exponent: int = 1):
         self.coeffient = coeffient
-        self.variables = variables
-
-    def clean(self) -> None:
-        delk = []
-        for k, v in self.variables.items():
-            if v == 0:
-                delk.append(k)
-        for k in delk:
-            del self.variables[k]
+        self.exponent = exponent
 
     @classmethod
     def read(cls, string: str) -> "Monomial":
         """Reads a monomial from a string. The string should be in the form of a number followed by one or more variables and their respective powers, ie."""
         string = str(string).lower().replace(" ", "")
         regex = re.compile(
-            r"(?P<coef>[+-]?[0-9]*)?(?P<variables>([a-zA-Z](\^[0-9]+|((\u00b2|\u00b3|\u00b9|\u2070|\u2074|\u2075|\u2076|\u2077|\u2078|\u2079)+)?))*)?"
+            r"(?P<coef>[+-]?[0-9]*)?(?P<variable>([a-zA-Z](\^[0-9]+|((\u00b2|\u00b3|\u00b9|\u2070|\u2074|\u2075|\u2076|\u2077|\u2078|\u2079)+)?))?)?"
         )
         match = regex.fullmatch(string)
         if match is None:
@@ -53,154 +44,98 @@ class Monomial:
             case _:
                 coef = int(coef_match or 1)
 
-        if not match.group("variables"):
-            return M(coef)
+        var = match.group("variable")
+        if not var:
+            return M(coef, 0)
+        var = var[1:].lstrip("^")
+        if not var:
+            return M(coef, 1)
 
-        variables = {}
-        current_var = None
-        current_num = 1
-        for char in string[len(coef_match or "") :]:
-            if re.fullmatch(r"[a-zA-Z]", char):
-                if current_var is not None:
-                    variables.setdefault(current_var, 0)
-                    variables[current_var] += (
-                        current_num if current_num is not None else 1
-                    )
-                current_var = char
-                current_num = None
-            elif char == "^":
-                pass
-            elif re.fullmatch(r"[0-9]", char):
-                if current_num is None:
-                    current_num = 0
-                current_num *= 10
-                current_num += int(char)
-            elif re.fullmatch(
-                r"(\u00b2|\u00b3|\u00b9|\u2070|\u2074|\u2075|\u2076|\u2077|\u2078|\u2079)",
-                char,
-            ):
-                if current_num is None:
-                    current_num = 0
-                current_num *= 10
-                current_num += _super_list.index(char)
-            else:
-                raise
-        if current_var:
-            variables.setdefault(current_var, 0)
-            variables[current_var] += current_num if current_num is not None else 1
-        mono = M(coef, **variables)
-        mono.clean()
-        return mono
+        normal = "".join(
+            map(lambda c: str(_super_list.index(c)) if c in _super_list else c, var)
+        )
 
-    def n_variables(self) -> int:
-        """How many unique variables are in this monomial."""
-        return len(self.variables)
+        return M(coef, int(normal))
 
-    @property
-    def is_constant(self) -> bool:
-        self.clean()
-        return not self.variables
+    def solve(self, value: float = 0) -> Generator[complex, None, None]:
+        omega_n = math.e ** (2j * math.pi / self.exponent)
 
-    def solve(self, var: str, value: float = 0) -> Generator[complex, None, None]:
-        power = self.variables[var]
-        omega_n = math.e ** (2j * math.pi / power)
-
-        for k in range(power):
-            yield (value / self.coeffient) ** (1 / power) * omega_n**k
+        for k in range(self.exponent):
+            yield (value / self.coeffient) ** (1 / self.exponent) * omega_n**k
 
     def derivative(self, n: int = 1) -> "Monomial":
         if n == 0:
             return self
-        self.clean()
 
         if self.is_constant:
             return M(0)
 
-        if len(self.variables) > 1:
-            raise ValueError()
-
-        var = list(self.variables.keys())[0]
         return Monomial.derivative(
-            M(self.coeffient * self.variables[var], **{var: self.variables[var] - 1}),
+            M(self.coeffient * self.exponent, self.exponent - 1),
             n - 1,
         )
 
     @property
-    def variable_list(self) -> list[str]:
-        return list(self.variables.keys())
+    def is_constant(self) -> bool:
+        return self.exponent == 0
 
-    def __call__(self, **values: float):
-        new = deepcopy(self)
-        for var in new.variable_list:
-            if var in values:
-                new.coeffient *= values[var] ** new.variables[var]
-                del new.variables[var]
+    def at(self, value: float):
+        return self.coeffient * value**self.exponent
 
-        return new
+    def __call__(self, value: int):
+        return self.at(value)
 
     def __eq__(self, other: Any) -> bool:
         return (
             isinstance(other, M)
             and self.coeffient == other.coeffient
-            and self.variables == other.variables
+            and self.exponent == other.exponent
         )
 
     def __add__(self, other: MonoCompat) -> "Monomial | Polynomial":
         other = ensure_monomial(other)
-        if self.variables == other.variables:
-            return M(self.coeffient + other.coeffient, **self.variables)
+        if self.exponent == other.exponent:
+            return M(self.coeffient + other.coeffient, self.exponent)
         return P([self, other])
 
     def __sub__(self, other: MonoCompat) -> "Monomial | Polynomial":
         other = ensure_monomial(other)
-        if self.variables == other.variables:
-            return M(self.coeffient - other.coeffient, **self.variables)
-        return P([self, other])
+        if self.exponent == other.exponent:
+            return M(self.coeffient - other.coeffient, self.exponent)
+        return P([self, M(-other.coeffient, other.exponent)])
 
     def __mul__(self, other: MonoCompat) -> "Monomial":
         other = ensure_monomial(other)
-        var_names = list(self.variables.keys()) + list(other.variables.keys())
-        variables = {
-            name: self.variables.get(name, 0) + other.variables.get(name, 0)
-            for name in var_names
-        }
-        return M(self.coeffient * other.coeffient, **variables)
+        return M(self.coeffient * other.coeffient, self.exponent + other.exponent)
 
     def __div__(self, other: MonoCompat) -> "Monomial":
         other = ensure_monomial(other)
-        var_names = list(self.variables.keys()) + list(other.variables.keys())
-        variables = {
-            name: self.variables.get(name, 0) - other.variables.get(name, 0)
-            for name in var_names
-        }
         ratio = self.coeffient / other.coeffient
         if not ratio.is_integer():
             raise ValueError("Coefficients not divisible")
-        return M(int(ratio), **variables)
+        return M(int(ratio), self.exponent - other.exponent)
 
     def __truediv__(self, other: "Monomial") -> "Monomial":
         return self.__div__(other)
 
     def __str__(self) -> str:
-        variable_names = sorted(self.variables.keys())
-        sorted_vars = {name: self.variables[name] for name in variable_names}
-        variables = "".join(
-            [
-                f"{name}{((_super_minus if power < 0 else "") + str(abs(power)).translate(_super)) if power != 1 else ""}"
-                for name, power in sorted_vars.items()
-            ]
-        )
-        coef_str = int(self.coeffient)
-
-        if all(power == 0 for _, power in self.variables.items()):
-            variables = ""
-        else:
-            if self.coeffient == 1:
+        match self.coeffient:
+            case 1:
                 coef_str = ""
-            elif self.coeffient == -1:
+            case -1:
                 coef_str = "-"
+            case _:
+                coef_str = str(self.coeffient)
 
-        return f"{coef_str}{variables}"
+        match self.exponent:
+            case 1:
+                exp_str = "x"
+            case 0:
+                exp_str = ""
+            case _:
+                exp_str = "x" + str(self.exponent).translate(_super)
+
+        return coef_str + exp_str
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -210,117 +145,103 @@ M = Monomial
 
 
 class Polynomial:
-    def __call__(self, value: int | None = None, **values):
-        if self.n_variables > 1 and value is not None:
-            raise ValueError(
-                "Cannot use position arguments with a polynomial with more than one unique variable"
-            )
-        elif value is not None:
-            var = self.elements[0].variable_list[0]
-            values.update({var: value})
-        new = deepcopy(self)
-        for i, e in enumerate(new.elements):
-            new.elements[i] = e(**values)
+    def __call__(self, value: float):
+        at = partial(Monomial.at, value=value)
+        return sum(map(at, self.elements))
 
-        new.clean()
-        return new
-    
-    def slope(self, )
+    def zero(self, value: float = 0, *, accuracy: int = 100, starting_guess: float = 0):
+        # gets a zero of a function using the Newton-Raphson
+        guess = starting_guess
+        slope = self.derivative()
 
-    def shell(self):
-        if not self.elements:
-            raise ValueError("Cannot shell a polynomial with no elements")
-        if self.n_variables > 1:
-            raise ValueError("Cannot shell a polynomial with more than one variable")
-
-        var: str = list(self.elements[0].variables.keys())[0]
-
-        current = deepcopy(self)
-        shelled: list[tuple[M, P, int]] = []
-
-        while len(current.elements) != 1:
-            if current.elements[-1].is_constant:
-                constant = current.elements[-1].coeffient
-                current = P(current.elements[:-1])
-            else:
-                constant = 0
-
-            factor = current.gc_mono(var)
-            if factor.coeffient == 1 and not factor.variables:
+        for _ in range(accuracy):
+            try:
+                guess -= self(guess) / slope(guess)
+            except ZeroDivisionError:
                 break
 
-            current /= factor
+        return guess
 
-            shelled.append((factor, current, constant))
+    def solve(self, value: float = 0):
+        """Solves a polynomial equation using the Newton-Raphson method and synthetic division."""
+        
+        zero = self.zero(value=value)
+        new = self.synthetic_division(zero)[0]
+        print("z", zero, "n", new)
+        
+        zero2 = new.zero(value=value)
+        new2 = new.synthetic_division(zero2)[0]
+        print("z", zero2, "n", new2)
 
-        return shelled
-    
-    def solve(self, var: str, value: float = 0, accuracy: int = 10):
-        guess = 5
+        zero3 = new2.zero(value=value)
+        new3 = new2.synthetic_division(zero3)[0]
+        print("z", zero3, "n", new3)
+        
 
+    def synthetic_division(self, value: float) -> tuple["Polynomial", int]:
+        """Finds the quotient and remainder of a polynomial division using synthetic division, in the form of P / (x - value)"""
+        under = [self.elements[0].coeffient]
 
+        for i, e in enumerate(self.elements[1:], start=1):
+            under.append(under[i - 1] * value + e.coeffient)
 
-    def gc_mono(self, var: str) -> "Monomial":
-        for e in self.elements:
-            e.clean()
-        coefficients = [e.coeffient for e in self.elements]
-        gcd_coef = math.gcd(*coefficients)
-        gcv = self.elements[-1].variables[var]
-        mono = M(gcd_coef, **{var: gcv})
-        mono.clean()
-        return mono
+        return P([M(coef, exp) for coef, exp in zip(under, range(self.degree)[::-1])]), under[-1]  # type: ignore
+
+    @property
+    def degree(self) -> int:
+        return max(e.exponent for e in self.elements)
+
+    def trim_zero(self):
+        for i, e in enumerate(self.elements):
+            if e.coeffient == 0:
+                del self.elements[i]
 
     def derivative(self, n: int = 1) -> "Polynomial":
         p = P(map(partial(Monomial.derivative, n=n), self.elements))
-        p.clean()
         return p
 
-    def clean(self):
-        map(Monomial.clean, self.elements)
-        self.elements = Polynomial(self.elements).elements
-        self.combine_like_terms()
-
-    def combine_like_terms(self):
-        for i, element in enumerate(self.elements):
-            element.clean()
-            changes = 0
-            for j, other in enumerate(self.elements[i + 1 :]):
-                if element.variables == other.variables:
-                    element.coeffient += other.coeffient
-                    del self.elements[j + i + 1 - changes]
-                    changes += 1
-
-    @property
-    def n_variables(self) -> int:
-        """
-        How many unique variables are in this polynomial.
-        """
-        unique = set()
-        for element in self.elements:
-            unique.update(element.variables.keys())
-        return len(unique)
+    def slope_line(self, x: float = 0) -> tuple[float, float]:
+        y = self(x)
+        slope = self.derivative()(x)
+        b = y - slope * x
+        return slope, b
 
     @classmethod
     def read(cls, string: str) -> "Polynomial":
-        # split on + or - to get monomials
-        elements = []
-        for element in string.split("+"):
-            if "-" in element:
-                elements.append(M.read(element.split("-")[0]))
-                for subelement in element.split("-")[1:]:
-                    elements.append(M.read("-" + subelement))
-            else:
-                elements.append(M.read(element))
+        segments = msplit(string, ["+", "-"], retain=["-"])
+
+        elements = list(map(M.read, segments))
+
         return cls(elements)
+    
+    def euc_div(self, other: "Polynomial") -> tuple["Polynomial", "Polynomial"]:
+        a = deepcopy(self)
+        b = deepcopy(other)
+
+        q = P([M(0)])
+        r = a
+        d = other.degree
+        c = other.lc
+
+        while r.degree >= d:
+            s =  M(1, r.degree - d) * M(r.lc / c, 1)
+            q += s
+            print(b, s, b*s)
+            r = r - b * s
+            print(r.degree, d)
+        return q, r
+
+
+
+    @property
+    def lc(self) -> float:
+        # sort by exponent
+        self.elements.sort(key=lambda e: e.exponent, reverse=True)
+        return self.elements[0].coeffient
 
     def __init__(self, elements: Iterable[Monomial]):
-        self.elements = sorted(
-            elements,
-            key=lambda e: (
-                max(power for _, power in e.variables.items()) if e.variables else 0
-            ),
-            reverse=True,
-        )
+        self.elements = list(elements)
+        self.elements.sort(key=lambda e: e.exponent, reverse=True)
 
     def __str__(self) -> str:
         if not self.elements:
@@ -330,7 +251,7 @@ class Polynomial:
             if element.coeffient > 0:
                 final += f" + {element}"
             else:
-                final += f" - {Monomial(-element.coeffient, **element.variables)}"
+                final += f" - {Monomial(-element.coeffient, element.exponent)}"
 
         return final
 
@@ -342,7 +263,7 @@ class Polynomial:
         copy = deepcopy(self)
         for oe in other.elements:
             for se in copy.elements:
-                if se.variables == oe.variables:
+                if se.exponent == oe.exponent:
                     se.coeffient += oe.coeffient
                     break
             else:
@@ -353,13 +274,13 @@ class Polynomial:
     def __sub__(self, other: PolyCompat):
         other = ensure_polynomial(other)
         copy = deepcopy(self)
-        for oe in copy.elements:
-            for se in other.elements:
-                if se.variables == oe.variables:
+        for oe in other.elements:
+            for se in copy.elements:
+                if se.exponent == oe.exponent:
                     se.coeffient -= oe.coeffient
                     break
             else:
-                copy.elements.append(M(-oe.coeffient, **oe.variables))
+                copy.elements.append(M(-oe.coeffient, oe.exponent))
 
         return copy
 
@@ -401,7 +322,35 @@ def ensure_polynomial(item: PolyCompat):
     return item
 
 
-# p = P.read("300x^10 + 3x^3 - 7x^2 + 20x + 2")
-# print(p(x=0))
-for s in M(10, x=4).solve("x", 10):
-    print(s)
+def msplit(string: str, delimiters: list[str], retain: list[str] | None = None):
+    """Splits a string by multiple delimiters, but retains the delimiters in the right side of the split."""
+    if retain is None:
+        retain = []
+
+    segments, current = list(), str()
+    current = ""
+    for char in string:
+        if char in delimiters:
+            segments.append(current)
+            current = char
+        elif char in retain:
+            current += char
+        else:
+            current += char
+
+    segments.append(current)
+    return segments
+
+
+# p = M.read("14x^2")
+# q = M.read("7x")
+# print(p)
+# print(q)
+# print(p + q)
+# print(p - q)
+# print(p * q)
+# print(p / q)
+
+p = P.read("x^2 - 5x -2")
+q = P.read("x - 1")
+print(p.euc_div(q))
